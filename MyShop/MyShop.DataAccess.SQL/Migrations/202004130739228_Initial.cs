13a8320bde05c4938a1f15101b76afdfcc5de1b0
ArjiBhogal2 <62878559+ArjiBhogal2@users.noreply.github.com>
2020-04-13 08:44:34 +0100

    Added Entity Framework to the solution

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202004130739228_Initial.cs
@@ -0,0 +1,43 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Description = c.String(),
+                        createddate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Category = c.String(),
+                        image = c.String(),
+                        createddate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+        }
+    }
+}
